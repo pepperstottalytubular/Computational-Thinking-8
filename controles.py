669ad815ccfc7c2deb584d082b3a491fd598a9d0
pepperstottalytubular <peppercameron@seattleacademy.org>
2025-02-27 17:14:20 +0000

    project 4 sections 1 and 2

--- controles.py
+++ controles.py
@@ -1,4 +1,20 @@
+# section one set up
+
 import codesters, random
 from codesters import StageClass
 stage = StageClass()
 stage.disable_floor()
+player=codesters.Sprite("tvgirl")
+stage.set_background("race")
+player.set_size(0.25)
+
+points=0
+#section 2- objects 
+def Falling_object():
+    global points
+    if points !=15:
+        y=250
+        x=random.randint(-250,250)
+        object=codesters.Sprite("star",x,y)
+        object.set_y_speed (-2)
+stage.event_interval(Falling_object,2)
